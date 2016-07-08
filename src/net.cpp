45b64ac7dd4d2e28c3df923ce5534b0e40290009
1111111 <111111111111>
2016-07-08 13:50:41 +0200

    nodes

--- src/net.cpp
+++ src/net.cpp
@@ -1133,8 +1133,12 @@ void MapPort()
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
 		    	{"node1.cryptojacks.com", "node1.cryptojacks.com"},
-                {"185.115.243.106", "185.115.243.106"},				
-                {"149.202.160.172", "149.202.160.172"},								
+                {"node2.cryptojacks.com", "node2.cryptojacks.com"},				
+                {"node3.cryptojacks.com", "node3.cryptojacks.com"},
+                {"node4.cryptojacks.com", "node4.cryptojacks.com"},	
+                {"node5.cryptojacks.com", "node5.cryptojacks.com"},	
+                {"node6.cryptojacks.com", "node6.cryptojacks.com"},	
+ 				{"node7.cryptojacks.com", "node7.cryptojacks.com"},	
 };
 
 void ThreadDNSAddressSeed(void* parg)
@@ -1205,14 +1209,11 @@ void ThreadDNSAddressSeed2(void* parg)
 
 unsigned int pnSeed[] =
 {
-    0xdf4bd379, 0x7934d29b, 0x26bc02ad, 0x7ab743ad, 0x0ab3a7bc,
-    0x375ab5bc, 0xc90b1617, 0x5352fd17, 0x5efc6c18, 0xccdc7d18,
-    0x443d9118, 0x84031b18, 0x347c1e18, 0x86512418, 0xfcfe9031,
-    0xdb5eb936, 0xef8d2e3a, 0xcf51f23c, 0x18ab663e, 0x36e0df40,
-    0xde48b641, 0xad3e4e41, 0xd0f32b44, 0x09733b44, 0x6a51f545,
-    0xe593ef48, 0xc5f5ef48, 0x96f4f148, 0xd354d34a, 0x36206f4c,
-    0xceefe953, 0x50468c55, 0x89d38d55, 0x65e61a5a, 0x16b1b95d,
-    0x702b135e, 0x0f57245e, 0xdaab5f5f, 0xba15ef63,
+    0xead49dc0, 0x29b4bd05, 0x2d956e4d, 0x956d995b, 0xd54886bc,
+    0x1a59c98a, 0x3c240092, 0x5352fd17, 0x33bdbd05, 0x62deaf4e,
+    0x7856235d, 0x56494863, 0x5a4c1457, 0xa807eed9, 0xa45c2332,
+    0x259f3f60, 0xf9ed013d, 0x33a3824b, 0xfa706905, 0xc63f19d4,
+    0x9cd26505,
 };
 
 void DumpAddresses()
